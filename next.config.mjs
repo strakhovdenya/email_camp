a4fe876f1423cbf3d53f8973b1ae34ed5581130a
strakhovdenya <strakhov.denya@gmail.com>
2025-05-17 13:03:02 +0300

    chore: optimize next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  images: {
+    domains: [],
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 export default nextConfig; 
