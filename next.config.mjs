bfd5cfbe007f1ee19a0bab17f3434eb50a0ddcd4
strakhovdenya <strakhov.denya@gmail.com>
2025-05-17 12:59:00 +0300

    fix: update dependencies and PWA configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
-import nextPWA from 'next-pwa';
-
-const withPWA = nextPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withPWA(nextConfig); 
+export default nextConfig; 
