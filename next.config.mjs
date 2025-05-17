5a3644c26bafa32c59ea08e546a13e8d0662eb96
strakhovdenya <strakhov.denya@gmail.com>
2025-05-17 12:51:31 +0300

    fix: add basic Next.js app structure and fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
-const withPWA = require('next-pwa')({
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development'
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+};
 
-module.exports = withPWA(nextConfig) 
+export default withPWA(nextConfig); 
