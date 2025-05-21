3e206fce0cc47454933102b8450e400f1671622d
strakhovdenya <strakhov.denya@gmail.com>
2025-05-21 13:29:13 +0200

    build: fix postcss-loader issue and successful build

--- next.config.js
+++ next.config.js
@@ -5,14 +5,7 @@ const nextConfig = {
   output: 'standalone',
   images: {
     unoptimized: true
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
   }
 };
- 
+
 export default nextConfig; 
